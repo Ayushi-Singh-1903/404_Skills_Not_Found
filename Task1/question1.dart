fe2941577bc9364f6cf7673386bf51ad664d0ae2
Ayushi-Singh-1903 <singhayushi1903@gmail.com>
2022-02-25 17:58:03 +0530

    Update question1.dart

--- Task1/question1.dart
+++ Task1/question1.dart
@@ -1,1 +1,12 @@
-
+import 'dart:io';
+void main() {
+  print("enter the number");
+  int? n= int.parse(stdin.readLineSync()!);
+  if(n%5==0) {
+    print("yes $n is divisible by 5");
+  }
+  else
+    {
+      print("$n is not divisible by 5");
+    }
+}
