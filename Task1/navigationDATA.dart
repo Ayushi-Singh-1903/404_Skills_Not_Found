0b172cb0449945dc4900ad3e193a5cb6bd993942
Ayushi-Singh-1903 <singhayushi1903@gmail.com>
2022-03-09 21:40:49 +0530

    Create navigationDATA.dart

---
+++ Task1/navigationDATA.dart
@@ -0,0 +1,40 @@
+Map<int, String> dataMap = {
+  1: 'assets/beast.jpg',
+  2: 'assets/nai.jpg',
+  3: 'assets/net.jpg',
+  4: 'assets/brent.jpg',
+  5: 'assets/david.jpg',
+  6: 'assets/lexi.jpg',
+};
+Map<int, String> dataMap1 = {
+  1: 'assets/mb1.jpg',
+  2: 'assets/n1.jpg',
+  3: 'assets/nn1.jpg',
+  4: 'assets/br1.jpg',
+  5: 'assets/dd1.jpg',
+  6: 'assets/ll1.jpg',
+};
+Map<int, String> dataMap2 = {
+  1: 'assets/mb2.jpg',
+  2: 'assets/n2.jpg',
+  3: 'assets/nn2.jpg',
+  4: 'assets/br2.jpg',
+  5: 'assets/dd2.jpg',
+  6: 'assets/ll2.jpg',
+};
+Map<int, String> dataMap3 = {
+  1: 'assets/mb3.jpg',
+  2: 'assets/n3.jpg',
+  3: 'assets/nn3.jpg',
+  4: 'assets/br3.jpg',
+  5: 'assets/dd3.jpg',
+  6: 'assets/ll3.jpg',
+};
+Map<int, String> dataMap4 = {
+  1: 'assets/mb4.jpg',
+  2: 'assets/n4.jpg',
+  3: 'assets/nn4.jpg',
+  4: 'assets/br4.jpg',
+  5: 'assets/dd4.jpg',
+  6: 'assets/ll4.jpg',
+};
